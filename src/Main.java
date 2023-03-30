e35b47a2dcf3d45c1adde60aaece4fb09810bd3a
Gustavo A. Rivera <123112483+gustavors1@users.noreply.github.com>
2023-03-30 20:38:03 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String[] nombres = {"Pepe", "Juan", "Nil", "Arthur"};
+        for (int i = 0; i < nombres.length; i++){
+            System.out.println(nombres[i]);
+        }
+    }
+}
